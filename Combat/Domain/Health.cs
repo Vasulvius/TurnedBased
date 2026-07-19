f079a44c66804b565be6845e736f33db7c479a1d
raphael_leurond <leurond.raphael@gmail.com>
2026-07-19 23:41:07 +0200

    feat: add Health value object

---
+++ Combat/Domain/Health.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Combat.Domain
+{
+    public sealed record Health
+    {
+        public int Max { get; }
+        public int Current { get; }
+        public bool IsDepleted => Current == 0;
+
+        private Health(int max, int current)
+        {
+            if (max <= 0)
+            {
+                throw new ArgumentException("MaxHealth must be positive", nameof(max));
+            }
+            if (current < 0)
+            {
+                throw new ArgumentException("CurrentHealth must not be negative", nameof(current));
+            }
+            if (max < current)
+            {
+                throw new ArgumentException(
+                    "MaxHealth must be higther or equal than CurrentHealth"
+                );
+            }
+            Max = max;
+            Current = current;
+        }
+
+        public static Health Create(int health)
+        {
+            return new Health(health, health);
+        }
+
+        public Health Reduce(int damage)
+        {
+            if (damage < 0)
+            {
+                throw new ArgumentException("Damage must not be negative", nameof(damage));
+            }
+            return new Health(Max, Math.Max(Current - damage, 0));
+        }
+    }
+}
