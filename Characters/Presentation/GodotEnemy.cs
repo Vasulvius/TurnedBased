805ac91f5972a33d2bdff54e91ebdf6be1e0753d
raphael_leurond <leurond.raphael@gmail.com>
2026-07-19 16:47:50 +0200

    feat: add animated enemy

---
+++ Characters/Presentation/GodotEnemy.cs
@@ -0,0 +1,23 @@
+using Godot;
+using Player.Domain.Models;
+
+public partial class GodotEnemy : Node2D
+{
+    [Export]
+    private AnimatedSprite2D[] _bodyParts;
+
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+
+    private void Attack()
+    {
+        foreach (AnimatedSprite2D bodyPart in _bodyParts)
+        {
+            if (bodyPart.SpriteFrames.HasAnimation(CharacterActions.Slash.ToString()))
+            {
+                bodyPart.Play(CharacterActions.Slash.ToString());
+            }
+        }
+    }
+}
